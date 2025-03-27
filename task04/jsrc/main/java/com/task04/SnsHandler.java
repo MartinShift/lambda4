a3d5c997b4d64f31d6a875236da96c8a36a8ffae
MartinShift <mori.steamer@gmail.com>
2025-03-27 21:24:09 +0100

    sisimasisi

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -4,15 +4,23 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
 @LambdaHandler(
     lambdaName = "sns_handler",
     roleName = "sns_handler-role",
     isPublishVersion = true,
-    aliasName = "${lambdas_alias_name}",
+    aliasName = "learn",
     logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
+
+@SnsEventSource(
+	targetTopic = "lambda_topic"
+)
+@DependsOn(resourceType = ResourceType.SNS_TOPIC, name = "lambda_topic")
 public class SnsHandler implements RequestHandler<SNSEvent, Void> {
     public Void handleRequest(SNSEvent event, Context context) {
         for (SNSEvent.SNSRecord record : event.getRecords()) {
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -4,16 +4,25 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
 @LambdaHandler(
     lambdaName = "sqs_handler",
     roleName = "sqs_handler-role",
     isPublishVersion = true,
-    aliasName = "${lambdas_alias_name}",
+    aliasName = "learn",
     logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
+@SqsTriggerEventSource(
+	targetQueue = "async_queue",
+	batchSize = 10
+)
+@DependsOn(resourceType = ResourceType.SQS_QUEUE, name = "async_queue")
 public class SqsHandler implements RequestHandler<SQSEvent, Void> {
+
     public Void handleRequest(SQSEvent event, Context context) {
         for (SQSEvent.SQSMessage message : event.getRecords()) {
             context.getLogger().log("Received message: " + message.getBody());
