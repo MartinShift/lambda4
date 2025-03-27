eaaad06faec0da6f0df7dd006b53e0f901189004
MartinShift <mori.steamer@gmail.com>
2025-03-27 20:47:38 +0100

    sisimasisi

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -3,7 +3,6 @@ package com.task04;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
@@ -13,10 +12,6 @@ import com.syndicate.deployment.model.RetentionSetting;
     isPublishVersion = true,
     aliasName = "${lambdas_alias_name}",
     logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
-)
-@SqsTriggerEventSource(
-    targetQueue = "async_queue",
-    batchSize = 10
 )
 public class SqsHandler implements RequestHandler<SQSEvent, Void> {
     public Void handleRequest(SQSEvent event, Context context) {
