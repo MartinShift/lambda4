538174d60c7b46bca1639b3c7ee44811c82da886
MartinShift <mori.steamer@gmail.com>
2025-03-27 19:46:54 +0100

    update

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.model.RetentionSetting;
 
 @LambdaHandler(
@@ -12,6 +13,9 @@ import com.syndicate.deployment.model.RetentionSetting;
     isPublishVersion = true,
     aliasName = "${lambdas_alias_name}",
     logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SnsEventSource(
+    targetTopic = "lambda_topic"
 )
 public class SnsHandler implements RequestHandler<SNSEvent, Void> {
     public Void handleRequest(SNSEvent event, Context context) {
