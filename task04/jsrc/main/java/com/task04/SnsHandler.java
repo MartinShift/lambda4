3188d49d42d8d798db90b21e7ba2f6f261242857
MartinShift <mori.steamer@gmail.com>
2025-03-27 22:23:31 +0100

    update

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -4,19 +4,10 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.resources.DependsOn;
-import com.syndicate.deployment.model.ResourceType;
-import com.syndicate.deployment.model.RetentionSetting;
 
-@LambdaHandler(
-    lambdaName = "sns_handler",
-    roleName = "sns_handler-role",
-    isPublishVersion = true,
-    aliasName = "learn",
-    logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
-)
-@DependsOn(resourceType = ResourceType.SNS_TOPIC, name = "lambda_topic")
+@LambdaHandler(lambdaName = "sns_handler", roleName = "sns_handler-role")
 public class SnsHandler implements RequestHandler<SNSEvent, Void> {
+
     public Void handleRequest(SNSEvent event, Context context) {
         for (SNSEvent.SNSRecord record : event.getRecords()) {
             context.getLogger().log("Received message: " + record.getSNS().getMessage());
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -4,18 +4,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.resources.DependsOn;
-import com.syndicate.deployment.model.ResourceType;
-import com.syndicate.deployment.model.RetentionSetting;
 
-@LambdaHandler(
-    lambdaName = "sqs_handler",
-    roleName = "sqs_handler-role",
-    isPublishVersion = true,
-    aliasName = "learn",
-    logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
-)
-@DependsOn(resourceType = ResourceType.SQS_QUEUE, name = "async_queue")
+@LambdaHandler(lambdaName = "sqs_handler", roleName = "sqs_handler-role")
 public class SqsHandler implements RequestHandler<SQSEvent, Void> {
 
     public Void handleRequest(SQSEvent event, Context context) {
