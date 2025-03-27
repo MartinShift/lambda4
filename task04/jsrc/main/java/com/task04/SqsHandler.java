fda2852bc4f067bb161517d9e4feae3dcb339b2e
MartinShift <mori.steamer@gmail.com>
2025-03-27 17:53:56 +0100

    init

---
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,26 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+@LambdaHandler(
+    lambdaName = "sqs_handler",
+    roleName = "sqs_handler-role",
+    isPublishVersion = true,
+    aliasName = "${lambdas_alias_name}",
+    logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+public class SqsHandler implements RequestHandler<SQSEvent, Void> {
+
+    public Void handleRequest(SQSEvent event, Context context) {
+        for (SQSEvent.SQSMessage message : event.getRecords()) {
+            // Log each message from the SQS queue
+            context.getLogger().log("Received message: " + message.getBody());
+            System.out.println("Message: " + message.getBody());
+        }
+        return null;
+    }
+}
